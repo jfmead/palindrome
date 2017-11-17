f9723e6b1eb1602a8718ea058c5b1bd8ad42911d
FuChai Mead <jessie.mf.wolff@gmail.com>
2017-11-17 10:20:14 -0700

    attempt at palindrome

---
+++ palindrome.rb
@@ -0,0 +1,23 @@
+require 'pry'
+
+
+def palindrome 
+  puts "------------------------"
+  puts "I can check to see if your word is a palindrome."
+  puts "What is the word? "
+  word = gets.strip.downcase
+  letters = word.scan(/\w/)
+
+  if letters.first == letters.last 
+    letters.pop()
+    letters.shift()
+    #need to make a loop to keep checking first and last letters 
+    #probably would create a function that checks letters 
+    puts "#{word} is a palindrome."
+  else
+    puts "#{word} is not a palindrome."
+ 
+palindrome
+end
+
+palindrome
